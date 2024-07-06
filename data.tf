8aaa743fb62b3d0f4c754a565ceeba34ff4447cd
iam-raja <rajapeta45@gmail.com>
2024-07-06 09:23:19 +0530

    cicd

--- data.tf
+++ data.tf
@@ -7,3 +7,13 @@ data "aws_ami" "ami_info" {
      values = ["RHEL-9-DevOps-Practice"]
   }
 }
+
+data "aws_ami" "nexus_ami_info" {
+  most_recent = true
+  owners = ["679593333241"]
+
+    filter {
+     name   = "name"
+     values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-af3afa48-*"]
+  }
+}
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "jenkins" {
 
   name = "jenkins"
 
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-03ec942beb955be40"]
   subnet_id              = "subnet-0c35fdd17ccc49343"
   user_data = file("jenkins.sh")
@@ -21,7 +21,7 @@ module "jenkins-agent" {
 
   name = "jenkins-agent"
 
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-03ec942beb955be40"]
   subnet_id              = "subnet-0c35fdd17ccc49343"
   user_data = file("jenkins-agent.sh")
@@ -33,6 +33,28 @@ module "jenkins-agent" {
     Environment = "dev"
   }
 }
+
+# resource "aws_key_pair" "nexus" {
+#   key_name   = "nexus"
+#   public_key = file("C:/devops/dev-trainings/dev-training-key.pub")
+# }
+
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "nexus"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = ["sg-03ec942beb955be40"]
+#   subnet_id              = "subnet-0c35fdd17ccc49343"
+#   ami = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.nexus.key_name
+
+#   tags = {
+#     Name = "nexus"
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 3.0"
@@ -55,6 +77,14 @@ module "records" {
       records = [
         module.jenkins-agent.private_ip
       ]
+    # },
+    #     {
+    #   name    = "nexus"
+    #   type    = "A"
+    #   ttl     = 1
+    #   records = [
+    #     module.nexus.private_ip
+    #   ]
     }
   ]
 }
